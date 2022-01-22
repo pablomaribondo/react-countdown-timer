8c06f2500aecc0fb21bd1dbf1d3fa5d29a1d094a
Pablo Maribondo <pablomgvieira@gmail.com>
2022-01-22 13:43:18 -0300

    feat: create countdown component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import { FC } from 'react';
 
 import './App.scss';
 
-const App: FC = () => {
+export const App: FC = () => {
   return (
     <div>
       <p>React Countdown Timer</p>
     </div>
   );
 };
-
-export default App;
