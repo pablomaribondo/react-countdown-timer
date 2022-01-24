f5a0a38bbaf37372dfdcfbc8347f000c3999cf67
Pablo Maribondo <pablomgvieira@gmail.com>
2022-01-23 22:02:32 -0300

    feat: update app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 
+import { Countdown } from './components/Countdown';
+
 import './App.scss';
 
 export const App: FC = () => {
   return (
-    <div>
-      <p>React Countdown Timer</p>
+    <div className="wrapper">
+      <Countdown date="August 31 2021 10:00 GMT+2" />
     </div>
   );
 };
